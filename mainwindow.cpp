a8c621cea3a7d3526ae8f84f15509d763edc2bd4
Dmitrii Ivanushkin <dmitrii.ivanushkin1@gmail.com>
2023-03-04 19:14:05 +0100

    build: All files with bug replaced

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,11 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include "QLabel"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    QLabel* scoreLabel = new QLabel("Score: 0");
-    statusBar()->addPermanentWidget(scoreLabel);
 }
 
 MainWindow::~MainWindow()
