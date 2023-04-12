cc7bfc9387f077c63426d417ed121ef4f2908560
0x21q <xkrato67@stud.fit.vutbr.cz>
2023-04-13 01:42:01 +0200

    feat(CharacterModel.cpp): Adding direction, position methods

--- src/Model/CharacterModel.cpp
+++ src/Model/CharacterModel.cpp
@@ -3,38 +3,67 @@
 /**
  * Character class
  */
-Character::Character(int speed) {
-    this->speed = speed;
+Character::Character() {
 
     std::cout << "[+] Character object created" << std::endl;
 }
 
-void Character::set_position(Position pos) {
+void Character::start_position(Position pos) {
     this->pos = pos;
 
-    std::cout << "Character position set" << std::endl;
+    std::cout << "Start position set" << std::endl;
+}
+
+Direction Character::get_direction() {
+    return this->dir;
+}
+
+void Character::set_direction(Direction dir) {
+    this->dir = dir;
+
+    std::cout << "Direction set" << std::endl;
+}   
+
+int Character::get_position_x() {
+    return this->pos.x;
+}
+int Character::get_position_y() {
+    return this->pos.y;
+}
+
+void Character::update_position() {
+    switch (this->dir) {
+    case Direction::UP:
+        this->pos.y--;
+        break;
+    case Direction::DOWN:
+        this->pos.y++;
+        break;
+    case Direction::LEFT:
+        this->pos.x--;
+        break;
+    case Direction::RIGHT:
+        this->pos.x++;
+        break;
+    default:
+        break;
+    }
+
+    std::cout << "Position updated" << std::endl;
 }
 
 /**
  * Pacman class
  */
-Pacman::Pacman(int speed, int health) : Character(speed) {
+Pacman::Pacman(int health) {
     this->health = health;
 
     std::cout << "[+] Pacman object created" << std::endl;
 }
 
-void Pacman::Move() {
-    std::cout << "Pacman moved" << std::endl;
-}
-
 /**
  * Ghost class
  */
-Ghost::Ghost(int speed) : Character(speed) {
+Ghost::Ghost() {
     std::cout << "[+] Ghost object created" << std::endl;
-}
-
-void Ghost::Move() {
-    std::cout << "Ghost moved" << std::endl;
 }
